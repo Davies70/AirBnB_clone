d0b24021bfe3001ee98191ae78fd40c749e31965
Ajayi <ajayidavies7@gmail.com>
2022-09-03 14:30:37 -0700

    clone w1

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ===========
 Airbnb_clone
 ===========
-A project that clones the Airbnb websites
+A project that clones the Airbnb website
 
 ---Console:
 A console is a command interpereter needed to manage (create, update, destroy, etc) objects, store and persist objects to a JSON file. The consle also helps with the manipulation and validation of the storage system.
--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,42 @@
 #!/usr/bin/python3
 
-""" base model module """
+""" base module """
 
-class BaseModule
+
+from datetime import datetime
+from uuid import uuid4
+
+
+class BaseModel:
+
+    """ defines all common attributes/methods for other classes """
+
+    def __init__(self):
+
+        """ initializing the class """
+
+        self.updated_at = datetime.today()
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+
+    def __str__(self):
+        """ defining string representation of class """
+
+        return ("[{}] ({}) {}"
+                .format(self.__class__.__name__, self.id, self.__dict__))
+
+    def save(self):
+
+        """ updating the datetime of object change """
+
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+
+        """ creating a dictionary format of the class """
+
+        my_dict = self.__dict__.copy()
+        my_dict['__class__'] = self.__class__.__name__
+        my_dict['updated_at'] = self.updated_at.isoformat()
+        my_dict['created_at'] = self.created_at.isoformat()
+        return my_dict
